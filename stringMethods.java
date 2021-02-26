756544311cabe79a3828ad13da7b370e2dbe8683
İrem <69118634+lastirembender@users.noreply.github.com>
2021-02-26 21:01:52 +0300

    Create stringMethods.java

---
+++ stringMethods.java
@@ -0,0 +1,14 @@
+public class stringMethods {
+	
+	public static void main(String[] args) {
+		String str = "the quick brown fox jumps over the lazy dog";
+		String str1 = "1";
+		
+		System.out.println(str.length());
+		System.out.println(str.indexOf("q"));
+		System.out.println(str.concat(str1));
+		System.out.println(str+str1);
+		
+	  }
+
+}
