0262d9b2c847ef1ca32f898194a45324af24c00c
İrem <69118634+lastirembender@users.noreply.github.com>
2021-02-26 20:32:57 +0300

    Create 6-operations.java

---
+++ 6-operations.java
@@ -0,0 +1,16 @@
+public class operations {
+	public static void main(String[] args) {
+		/* Java'da çeşitli aritmetik operansonlar kullanılır. Bunlar:
+		 + toplama, - çıkarma, * çarpma, / bölme, % moduler aritmetik
+		 ++ 1 arttırma, -- 1 eksiltme
+		 */
+		int a = 5;
+		int birarttir = a++;
+		int birazalt = --a;
+		double modul = a%2;
+		
+		System.out.println(a);
+		System.out.println(modul);
+	  }
+
+}
