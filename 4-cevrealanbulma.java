52613df26e832ad51bb6d8a029cd9f311023a52d
İrem <69118634+lastirembender@users.noreply.github.com>
2021-02-26 20:19:04 +0300

    Create 4-cevrealanbulma.java

---
+++ 4-cevrealanbulma.java
@@ -0,0 +1,10 @@
+public class Merhaba {
+  
+	public static void main(String[] args) {
+		double r=5.5;
+		double cevre=2*3.14*r;
+		double alan=3.14*r*r;
+		System.out.println(cevre);
+		System.out.println(alan);
+	  }
+}
