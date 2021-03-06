659585ed1451015d959e796dbbd57e81fa0cbdc7
İrem <69118634+lastirembender@users.noreply.github.com>
2021-03-06 21:20:44 +0300

    Create 9- inputAlma.java

---
+++ 9- inputAlma.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class inputAlma {
+	public static void main(String[] args) {
+		System.out.println("Bir sayi giriniz:");
+		Scanner klavye = new Scanner(System.in);
+		int sayi = klavye.nextInt();
+		System.out.println("Girdiginiz sayi:"+sayi);
+	  }
+
+}
